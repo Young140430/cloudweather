ba336f5b0efcd7e5dbc6fe91266c2b3e626a94ba
Young <2109337821@qq.com>
2020-11-26 22:41:38 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/example/cloudweather/db/City.java
+++ app/src/main/java/com/example/cloudweather/db/City.java
@@ -6,7 +6,7 @@ public class City extends LitePalSupport {
     private int id;
     private String cityName;
     private int cityCode;
-    private int proviceId;
+    private int provinceId;
 
     public int getId() {
         return id;
@@ -32,11 +32,11 @@ public class City extends LitePalSupport {
         this.cityCode = cityCode;
     }
 
-    public int getProviceId() {
-        return proviceId;
+    public int getProvinceId() {
+        return provinceId;
     }
 
-    public void setProviceId(int proviceId) {
-        this.proviceId = proviceId;
+    public void setProvinceId(int proviceId) {
+        this.provinceId = proviceId;
     }
 }
